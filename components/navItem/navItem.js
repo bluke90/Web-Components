88bbd2928bb6b7091b17b914663070f1aee3f0f5
Blake Ganzerla <btg151@msstate.edu>
2024-08-09 10:48:29 -0500

    Pushed Nav menu and items

---
+++ components/navItem/navItem.js
@@ -0,0 +1,32 @@
+﻿class NavItem extends HTMLElement {
+    constructor() {
+        super();
+
+        // create a shadow root
+        this.shadow = this.attachShadow({mode: 'closed'});
+
+        // get the template
+        this._template = importer.templates.find(c => c.component === 'navItem');
+        this.shadow.append(this._template.template.content.cloneNode(true));
+        
+        this._anchor = this.shadow.querySelector("#nav-link");
+        this._label = this.textContent;
+        
+        
+        this._anchor.innerText = this._label;
+
+    }
+    static get observedAttributes(){
+        return ["link"];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue){
+        if(oldValue === newValue) return;
+        switch(name){
+            case "link":
+                if(newValue) this._anchor.href = newValue;
+                break;
+        }
+    }
+}
+customElements.define("nav-item", NavItem);
---
+++ components/navMenu/navMenu.js
@@ -0,0 +1,47 @@
+﻿class NavMenu extends HTMLElement {
+    constructor() {
+        super();
+
+        // create a shadow root
+        this.shadow = this.attachShadow({mode: 'closed'});
+
+        // get the template
+        this._template = importer.templates.find(c => c.component === 'navMenu');
+        this.shadow.append(this._template.template.content.cloneNode(true));
+        
+        this._toggle = this.shadow.querySelector("#menu-toggle-btn");
+        this._toggle.addEventListener("click", this.toggleMenu.bind(this));
+        this._menu = this.shadow.querySelector(".wrapper");
+    }
+    
+    static get observedAttributes(){
+        return ["background-color", "top"];
+    }
+
+    connectedCallback(){
+        let toolbar = document.querySelector(".toolbar");
+        if (toolbar) {
+            this._toggle.style.top = "-4rem";
+        }
+
+    }
+    
+    attributeChangedCallback(name, oldValue, newValue){
+        if(oldValue === newValue) return;
+        switch(name){
+            case "background-color":
+                if(newValue) this._menu.style.backgroundColor = newValue;
+                break;
+            case "top":
+                if(newValue) this._menu.style.top = newValue;
+                break;
+        }
+    }
+    
+    toggleMenu(){
+        this._menu.classList.toggle("open");
+        this._toggle.classList.toggle("open");
+    }
+
+}
+customElements.define("nav-menu", NavMenu);
